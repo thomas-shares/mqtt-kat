eaf2cbb70d92f85ec6e377709a9478f7d9d7757e
jaeyeon <jaeyeon@smarf.kr>
2020-08-25 22:21:29 +0900

    Refactoring handling disconnect packet.

--- src/mqttkat/handlers/disconnect.clj
+++ src/mqttkat/handlers/disconnect.clj
@@ -1,22 +1,22 @@
 (ns mqttkat.handlers.disconnect
   (:require [mqttkat.s :refer [*server*]]
             [mqttkat.handlers :refer :all])
-  (:import [org.mqttkat.packages MqttDisconnect]
-           [org.mqttkat.server MqttServer]))
+  (:import [org.mqttkat.server MqttServer]))
 
 (defn disconnect-client [client-key]
-  (logger "Disconnecting client " client-key)
-  (swap! *clients* dissoc client-key)
-  (logger (keys @*clients*))
-  (let [s (:server (meta @*server*))]
-    (.closeConnection ^MqttServer s client-key)))
+  (do
+    (logger "Disconnecting client " client-key)
+    (swap! *clients* dissoc client-key)
+    (logger (keys @*clients*))
+    (let [{s :server} (meta @*server*)]
+      (.closeConnection ^MqttServer s client-key))))
 
+(defn disconnect
+  [{:keys [client-key] :as msg}]
+  (do
+    (logger "clj DISCONNECT received: " msg)
+    (logger "before swap " (keys @*clients*))
+    (swap! *clients* dissoc client-key)
+    (logger "after swap " (keys @*clients*))
+    (disconnect-client client-key)))
 
-(defn disconnect [msg]
-  (logger "clj DISCONNECT received: " msg)
-  ;(println "count: " (count (get @subscribers "test")))
-  ;(swap! sub2 tr/delete-matching  (:client-key msg))
-  (logger "before swap " (keys @*clients*))
-  (swap! *clients* dissoc (:client-key msg))
-  (logger "after swap " (keys @*clients*))
-  (disconnect-client (:client-key msg)))
