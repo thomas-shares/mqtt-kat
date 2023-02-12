28d1d338df2817988c729d9d7434b75a8627cc0e
Thomas van der Veen <thomas@bluefeniks.com>
2023-02-12 20:24:48 +0100

    add deps.edn, first attempt

--- project.clj
+++ project.clj
@@ -8,16 +8,16 @@
   :global-vars {*warn-on-reflection* true}
 
   :dependencies
-  [[org.clojure/clojure "1.10.1"]
-   [org.clojure/core.async "1.3.610"]
-   [org.clojure/spec.alpha "0.2.187"]
-   [org.clojure/core.specs.alpha "0.2.44"]
-   [org.clojure/test.check "1.1.0"]
+  [[org.clojure/clojure "1.11.1"]
+   [org.clojure/core.async "1.5.648"]
+   [org.clojure/spec.alpha "0.3.218"]
+   [org.clojure/core.specs.alpha "0.2.62"]
+   [org.clojure/test.check "1.1.1"]
    [org.craigandera/causatum "0.3.0"]
    [clojurewerkz/triennium "1.0.0-beta2"]
    [overtone/at-at "1.2.0"]
    ;;[djblue/portal "0.6.1"]
-   [com.clojure-goes-fast/clj-async-profiler "0.4.1"]]
+   [com.clojure-goes-fast/clj-async-profiler "0.5.1"]]
 
   :jvm-opts
   ["-Dclojure.compiler.disable-locals-clearing=true"
